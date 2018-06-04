6877f3b4623397c6fe1eb481a769ec4fab0ce9b3
pebeto <pebeto99@gmail.com>
2018-06-04 13:19:13 -0500

    Implementado algoritmo de búsqueda en recuadro más eficiente

--- funcionesSudoku.h
+++ funcionesSudoku.h
@@ -25,24 +25,72 @@ bool encontrarVacio(int &x, int &y, int sudoku[9][9]){
 }
 
 bool buscarMinMatriz(int i, int x, int y, int sudoku[9][9]){
+    if(x%3==0 && y%3==0)                                    // Algoritmo de búsqueda en cuadro más eficiente
+        for(int p=1; p<3; p++)
+            for(int q=1; q<3; q++)
+                if(sudoku[x+p][y+q]==i)
+                    return true;
+    if(x%3==0 && y%3==1)
+        for(int p=1; p<3; p++)
+            for(int q=0; q<3; q=q+2)
+                if(sudoku[x+p][y-1+q]==i)
+                    return true;
+    if(x%3==0 && y%3==2)
+        for(int p=1; p<3; p++)
+            for(int q=0; q<2; q++)
+                if(sudoku[x+p][y-2+q]==i)
+                    return true;
+    if(x%3==1 && y%3==0)
+        for(int p=0; p<3; p=p+2)
+            for(int q=1; q<3; q++)
+                if(sudoku[x-1+p][y+q]==i)
+                    return true;
+    if(x%3==1 && y%3==1)
+        for(int p=0; p<3; p=p+2)
+            for(int q=0; q<3; q=q+2)
+                if(sudoku[x-1+p][y-1+q]==i)
+                    return true;
+    if(x%3==1 && y%3==2)
+        for(int p=0; p<3; p=p+2)
+            for(int q=0; q<2; q++)
+                if(sudoku[x-1+p][y-2+q]==i)
+                    return true;
+    if(x%3==2 && y%3==0)
+        for(int p=0; p<2; p++)
+            for(int q=1; q<3; q++)
+                if(sudoku[x-2+p][y+q]==i)
+                    return true;
+    if(x%3==2 && y%3==1)
+        for(int p=0; p<2; p++)
+            for(int q=0; q<3; q=q+2)
+                if(sudoku[x-2+p][y-1+q]==i)
+                    return true;
+    if(x%3==2 && y%3==2)
+        for(int p=0; p<2; p++)
+            for(int q=0; q<2; q++)
+                if(sudoku[x-2+p][y-2+q]==i)
+                    return true;
+
+    /* Algoritmo de búsqueda en cuadro menos eficiente
 	if(x%3==0 && y%3==0 && (sudoku[x+1][y+1]==i || sudoku[x+1][y+2]==i || sudoku[x+2][y+1]==i || sudoku[x+2][y+2]==i))
-        return true;
-	if(x%3==1 && y%3==0 && (sudoku[x-1][y+1]==i || sudoku[x-1][y+2]==i || sudoku[x+1][y+1]==i || sudoku[x+1][y+2]==i))
-        return true;
-	if(x%3==2 && y%3==0 && (sudoku[x-2][y+1]==i || sudoku[x-2][y+2]==i || sudoku[x-1][y+1]==i || sudoku[x-1][y+2]==i))
         return true;
 	if(x%3==0 && y%3==1 && (sudoku[x+1][y-1]==i || sudoku[x+2][y-1]==i || sudoku[x+1][y+1]==i || sudoku[x+2][y+1]==i))
         return true;
  	if(x%3==0 && y%3==2 && (sudoku[x+1][y-2]==i || sudoku[x+1][y-1]==i || sudoku[x+2][y-2]==i || sudoku[x+2][y-1]==i))
         return true;
+	if(x%3==1 && y%3==0 && (sudoku[x-1][y+1]==i || sudoku[x-1][y+2]==i || sudoku[x+1][y+1]==i || sudoku[x+1][y+2]==i))
+        return true;
     if(x%3==1 && y%3==1 && (sudoku[x-1][y-1]==i || sudoku[x-1][y+1]==i || sudoku[x+1][y-1]==i || sudoku[x+1][y+1]==i))
         return true;
     if(x%3==1 && y%3==2 && (sudoku[x-1][y-2]==i || sudoku[x-1][y-1]==i || sudoku[x+1][y-2]==i || sudoku[x+1][y-1]==i))
+        return true;
+	if(x%3==2 && y%3==0 && (sudoku[x-2][y+1]==i || sudoku[x-2][y+2]==i || sudoku[x-1][y+1]==i || sudoku[x-1][y+2]==i))
         return true;
     if(x%3==2 && y%3==1 && (sudoku[x-2][y-1]==i || sudoku[x-1][y-1]==i || sudoku[x-2][y+1]==i || sudoku[x-1][y+1]==i))
         return true;
     if(x%3==2 && y%3==2 && (sudoku[x-2][y-2]==i || sudoku[x-2][y-1]==i || sudoku[x-2][y-1]==i || sudoku[x-1][y-1]==i))
         return true;
+        */
     return false;
 }
 
@@ -62,7 +110,7 @@ bool resolverSudoku(string nombre,int sudoku[9][9]){
 		return true;
     for(int i=1; i<=9; i++){
         if(!seEncuentra(i, x, y, sudoku)){
-            //usleep(1000000);
+            //usleep(100000);
             sudoku[x][y]= i;
             system("clear");
             cout<<nombre<<endl;
